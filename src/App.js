26d4bf8f78d0d57f8d193369b25ba3da7b1c5508
Yemisirach <yemsrach3723@gmail.com>
2023-02-27 21:45:00 +0300

    correct eslint error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Hello microvers
+        <div>Hello world</div>
       </header>
     </div>
   );
