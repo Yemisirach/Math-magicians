2e80859e315df504c8c2338198c5f60c97c3a454
Yemisirach <yemsrach3723@gmail.com>
2023-03-08 10:59:59 +0300

    Add home and quote page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div id="footer">
       <h2>Math Magician project</h2>
-      <h3>By, Yemisirach Tamirat.</h3>
+      <h3>By:- Yemisirach Tamirat.</h3>
     </div>
   );
 }
